7daa19afdc9b3eeb736e9c7e8a73458c7cd8a615
Asif Estiak <me.asif.ae@gmail.com>
2021-02-28 02:07:01 +0600

    added items in header section

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,8 +6,13 @@ const Header = () => {
   return (
     <div className="header">
       <div className="main-logo">
-        <img src={logo} alt=""/>
+        <a href="/"><img src={logo} alt=""/></a>
       </div>
+      <nav className="navbar">
+        <a href="/shop"><div className="nav-div">Shop</div></a>
+        <a href="/review"><div className="nav-div">Order Review</div></a>
+        <a href="/manage"><div className="nav-div">Manage Inventory</div></a>
+      </nav>
     </div>
   );
 };
